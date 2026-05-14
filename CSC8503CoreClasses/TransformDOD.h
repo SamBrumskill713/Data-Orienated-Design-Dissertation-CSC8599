778eefad5e3575d61a6400a8df2d47842098b75b
SamBrumskill713 <C5055756@newcastle.ac.uk>
2026-05-14 21:26:40 +0100

    Converted transforms

---
+++ CSC8503CoreClasses/TransformDOD.h
@@ -0,0 +1,34 @@
+#pragma once
+using namespace NCL::Maths;
+namespace NCL {
+	namespace CSC8503 {
+		struct TransformsComp {
+			Matrix4 matrix;
+			Quaternion orientation;
+			Vector3 position;
+			Vector3 scale = Vector3(1, 1, 1);
+		};
+
+		struct TransformsSys {
+			TransformsComp transformData;
+
+			TransformsComp& SetPosition(const Vector3& worldPos);
+			TransformsComp& SetScale(const Vector3& worldScale);
+			TransformsComp& SetOrientation(const Quaternion& newOr);
+
+			Vector3 GetPosition() {
+				return transformData.position;
+			}
+
+			Quaternion GetOrientation() {
+				return transformData.orientation;
+			}
+
+			Matrix4 GetMatrix() {
+				return transformData.matrix;
+			}
+		private:
+			void UpdateMatrix();
+		};
+	}
+}
